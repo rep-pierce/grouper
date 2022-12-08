80a9b34bf71173bbc15302ade26be88482ad8296
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-12-07 20:37:51 -0700

    update

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :usersgroups
+  has_many :users, through: :usersgroups
+  has_many :posts
+  has_many :users, through: :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :groups
+    has_many :usersgroups
+    has_many :groups, through: :usersgroups
+    has_many :posts
+    has_many :groups, through: :posts
 end
