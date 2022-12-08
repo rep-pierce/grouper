8fabf456734349f2fba5e90567d96e5701cdcb5e
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-12-07 23:15:28 -0700

    update

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :usersgroups, dependent: :destroy
-  has_many :users, through: :usersgroups, dependent: :destroy
+  has_many :users_groups, dependent: :destroy
+  has_many :users, through: :users_groups, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :users, through: :posts, dependent: :destroy
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :groups, dependent: :destroy
-    has_many :usersgroups, dependent: :destroy
-    has_many :groups, through: :usersgroups, dependent: :destroy
+    has_many :users_groups, dependent: :destroy
+    has_many :groups, through: :users_groups, dependent: :destroy
     has_many :posts, dependent: :destroy
     has_many :groups, through: :posts, dependent: :destroy
 end
