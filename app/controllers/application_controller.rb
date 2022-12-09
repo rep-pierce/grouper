80e874b967f42d0eeff254bcc76f3a9cf09f14ed
Grace Durant <gdurant34@gmail.com>
2022-12-09 01:23:05 -0800

    backend login and auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authorized
+    # skip_before_action :authorized, only: :index
+    def authorized
+        return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
+    end
 end
